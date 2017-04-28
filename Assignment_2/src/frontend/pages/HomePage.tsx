db17fd9340cb770f9bc181b52270f75936f5b01c
Patrick Shaw <Patrick.Leong.Shaw@gmail.com>
2017-04-28 21:41:21 +1000

    Fixed missing files error after David's merge

---
+++ Assignment_2/src/frontend/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+class HomePage extends React.Component<void, void> {
+    constructor() {
+        super();
+    }
+  render() {
+    return (
+      <h1>:3</h1>
+    );
+  }
+}
+export {HomePage};
+export default HomePage;
--- README.md
+++ README.md
@@ -1,8 +1,2 @@
-# FIT3077-Full-Lambda => {}
+# FIT3077-Full-Lambda
 FIT3077 Assignment 2.
-
-Patrick Shaw
-
-David Lei
-
-Documentation style guide: https://github.com/airbnb/javascript
