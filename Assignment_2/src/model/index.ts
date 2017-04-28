917c6329bc11320becea2b2e9798cd53b8a63f6c
darvid7 <bvdarvid@gmail.com>
2017-04-28 18:59:31 +1000

    wipe repo

--- Assignment_2/src/model/index.ts
+++
@@ -1,6 +0,0 @@
-import {TemperatureData} from './TemperatureData';
-import {RainfallData} from './RainfallData';
-import {WeatherLocationData} from './WeatherLocationData';
-export {TemperatureData};
-export {RainfallData};
-export {WeatherLocationData};
