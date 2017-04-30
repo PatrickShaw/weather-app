486ca580e09d680a6fe909715f4074bfafd67058
darvid7 <bvdarvid@gmail.com>
2017-05-01 02:44:11 +1000

    Don't render units for no data, add rainfall units

--- Assignment_2/src/frontend/components/MonitoringItem.tsx
+++ Assignment_2/src/frontend/components/MonitoringItem.tsx
@@ -7,17 +7,33 @@ interface MonitoringItemProps {
 }
 class MonitoringItem extends React.Component<MonitoringItemProps, void> {
   public render(): JSX.Element {
+
+    let temperatureDataToRender: string = 'N/A';
+    if (this.props.weatherData.temperatureData) {
+      // Don't render ℃ with N/A.
+      if (this.props.weatherData.temperatureData.temperature !== 'N/A') {
+        temperatureDataToRender = this.props.weatherData.temperatureData.temperature + ' ℃';
+      }
+    }
+
+    let rainfallDataToRender: string = 'N/A';
+    if (this.props.weatherData.rainfallData) {
+      if (this.props.weatherData.rainfallData.rainfall !== 'N/A') {
+        rainfallDataToRender = this.props.weatherData.rainfallData.rainfall + ' mm';
+      }
+    }
+
     return (
       <section className="pad-item-list">
         <h1 className="txt-body-2">{this.props.weatherData.location}</h1>
         {
           this.props.weatherData.rainfallData ? 
-          <h2 className="txt-body-1">{this.props.weatherData.rainfallData.rainfall}</h2> : 
+          <h2 className="txt-body-1">{rainfallDataToRender}</h2> : 
           null
         }
         {
           this.props.weatherData.temperatureData ? 
-          <h2 className="txt-body-1">{`${this.props.weatherData.temperatureData.temperature} ℃`}</h2> :
+          <h2 className="txt-body-1">{temperatureDataToRender}</h2> :
           null
         }
       </section>
