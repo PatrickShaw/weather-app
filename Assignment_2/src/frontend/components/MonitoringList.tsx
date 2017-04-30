978d1fa2bf3ebc7e8c05e3a8087d6e5333c80aac
Patrick Shaw <Patrick.Leong.Shaw@gmail.com>
2017-04-30 16:10:13 +1000

    Added cards styling to MonitoringList

--- Assignment_2/src/frontend/components/MonitoringList.tsx
+++ Assignment_2/src/frontend/components/MonitoringList.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 import { MonitoringItem } from './MonitoringItem';
 import { WeatherLocationData } from '../../model/WeatherLocationData';
-
+import './MonitoringList.scss';
 interface MonitoringListProps {
   weatherDataList: WeatherLocationData[];
 }
@@ -10,11 +10,11 @@ interface MonitoringListProps {
 class MonitoringList extends React.Component<MonitoringListProps, void> {
   public render(): JSX.Element {
     return (
-      <section>
+      <section className="monitoring-list">
         {
           this.props.weatherDataList.map((weatherDataItem: WeatherLocationData, weatherIndex: number) => {
             return (
-              <div key={weatherIndex} className="card">
+              <div key={weatherIndex} className="card monitoring-item-card">
                 <MonitoringItem weatherData={weatherDataItem}/>
               </div>
             );
