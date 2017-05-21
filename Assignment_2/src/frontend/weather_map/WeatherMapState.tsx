b223c27874a1fe7856f540cbca9fe4be21e51e60
darvid7 <bvdarvid@gmail.com>
2017-05-21 21:31:05 +1000

    Add marker and heatmap location for each location, reset locations on render

--- Assignment_2/src/frontend/weather_map/WeatherMapState.tsx
+++ Assignment_2/src/frontend/weather_map/WeatherMapState.tsx
@@ -2,6 +2,8 @@ import { LocationMarkerInformation } from './GoogleWeatherMap';
 
 class WeatherMapState {
   public locationInfo: LocationMarkerInformation[];
+  public locationPins: google.maps.Marker[] = [];
+  public locationHeatMap: google.maps.Circle[] = [];
   
   constructor(locationInfo: LocationMarkerInformation[]) {
     this.locationInfo = locationInfo;
@@ -10,6 +12,21 @@ class WeatherMapState {
   public setLocationInfo(locationInfo: LocationMarkerInformation[]) {
     this.locationInfo = locationInfo;
   }
+
+  public clearPins() {
+    for (const pin of this.locationPins) {
+      pin.setMap(null);
+    }
+    this.locationPins = [];
+  }
+
+  public clearCircles() {
+    for (const circle of this.locationHeatMap) {
+      circle.setMap(null);
+    }
+    this.locationHeatMap = [];
+  }
+
 }
 
 export { WeatherMapState };
