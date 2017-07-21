0d89c39c8bc22b2982b899010bd24564ba94b2de
Patrick Shaw <Patrick.Leong.Shaw@gmail.com>
2017-07-22 03:03:32 +1000

    AppBar, GenericListItem, MonitoringList now use SFCs

--- Assignment_2/src/frontend/view/AppBar.tsx
+++ Assignment_2/src/frontend/view/AppBar.tsx
@@ -9,16 +9,12 @@ interface ActionBarProps {
 /**
  * The action bar for Full Lambda
  */
-class ActionBar extends React.Component<ActionBarProps, {}> {
-  public render(): JSX.Element {
-    return (
-      <header className='app-bar'>
-        <h1 className='txt-app-bar-title light'>{this.props.title}</h1>
-        <h2 className='txt-app-bar-subtitle light'>{this.props.subtitle}</h2>
-      </header>
-    );
-  }
-}
+const ActionBar: React.SFC<ActionBarProps> = ({title, subtitle}) => (
+  <header className='app-bar'>
+    <h1 className='txt-app-bar-title light'>{title}</h1>
+    <h2 className='txt-app-bar-subtitle light'>{subtitle}</h2>
+  </header>
+);
 
 export default ActionBar;
 export {ActionBar};
